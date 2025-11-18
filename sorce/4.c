f8eaf33a0b2184f7f0056e76e5f80314790edbb9
1260315 <260315x@ugs.kochi-tech.ac.jp>
2025-11-18 11:50:07 +0000

    4.c end

--- sorce/4.c
+++ sorce/4.c
@@ -21,3 +21,73 @@ Linuxでは優先度とラウンドロビンを組み合わせたスケジュー
 #include <sys/wait.h>   // wait
 #include <unistd.h>     // nice
 #include <time.h>       // clock_gettime
+#include <errno.h>      // errno
+
+int main(void){
+
+    //開始時刻の取得
+    /*
+    struct timespec {
+    time_t   tv_sec;     　秒
+    long     tv_nsec;      ナノ秒
+    };
+    */
+    struct timespec before_ts;
+
+    clock_gettime(CLOCK_REALTIME, &before_ts);
+    printf("開始時刻(tv_nsec) : %ld\n", before_ts.tv_nsec);
+
+    pid_t pid;
+    int status;
+    int priority;
+
+    if((pid = fork()) < 0){           //forkで子プロセスの作成
+        perror("fork");
+        exit(1);
+
+    }else if(pid == 0){     //子プロセスである
+
+        //優先度設定-19に
+        if((priority = nice(19)) != 19){
+            perror("nice");
+        };
+
+        //空ループ
+        for(int i = 0; i <= 1000000; i++){}
+
+        //終了時刻を取得
+        struct timespec after_ts;
+        clock_gettime(CLOCK_REALTIME, &after_ts);
+
+        //終了時間・実行時間を出力
+        printf("\n子プロセス優先度 : %d\n子プロセス終了時刻(tv_nsec) : %ld\n子プロセス実行時間(nsec) : %ld\n", 
+            priority, after_ts.tv_nsec, after_ts.tv_nsec-before_ts.tv_nsec);
+
+        exit(0);
+
+    }else{                  //親プロセスである
+
+        //優先度設定19に
+        if((priority = nice(-19)) != -19){
+            perror("nice");
+        };
+
+        //空ループ
+        for(int i = 0; i <= 1000000; i++){}
+
+        //終了時刻を取得
+        struct timespec after_ts;
+        clock_gettime(CLOCK_REALTIME, &after_ts);
+
+        //終了時間・実行時間を出力
+        printf("\n親プロセス優先度 : %d\n親プロセス終了時刻(tv_nsec) : %ld\n親プロセス実行時間(nsec) : %ld\n", 
+            priority, after_ts.tv_nsec, after_ts.tv_nsec-before_ts.tv_nsec);
+
+        if(wait(&status) == (pid_t) - 1){   //子プロセスの終了を待つ
+            perror("wait");
+            exit(1);
+        }
+        exit(0);
+    }
+
+}
